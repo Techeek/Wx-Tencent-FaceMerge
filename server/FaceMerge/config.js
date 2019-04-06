07a5198d2ee55f154f40246431138a548da985e9
Techeek <qc_957a692741d9444b9f25c53a3da69e7a@git.code.tencent.com>
2019-04-06 23:00:21 +0800

    remove tencentAPIID

--- server/FaceMerge/config.js
+++ server/FaceMerge/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  SecretId: 'AKIDiD5k9xuSKZcnzGtjczStJYXM282k2Tgf', //腾讯云的SecretId，请替换成你自己的
-  SecretKey: 'DBmZd5TTUyIasbFi6H0ekyhCWZMBobGL' //腾讯云的SecretKey，请替换成你自己的
+  SecretId: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', //腾讯云的SecretId，请替换成你自己的
+  SecretKey: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' //腾讯云的SecretKey，请替换成你自己的
 };
