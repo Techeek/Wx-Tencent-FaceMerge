13edb40d58c17f74591b7543e3c42267f9b79e86
Techeek <qc_957a692741d9444b9f25c53a3da69e7a@git.code.tencent.com>
2019-04-08 13:28:57 +0800

    code comment

--- client/app.js
+++ client/app.js
@@ -1,3 +1,3 @@
-wx.cloud.init({
-  env: 'test-aa10b0'
+wx.cloud.init({ //云开发初始化环境
+  env: 'test-aa10b0' //更改为你自己的环境ID
 })
--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -1,52 +1,52 @@
 // client/pages/composite/composite.js
 Page({
   data: {
-    button_text: "上传照片",
-    image_src: "../../libs/img/man.jpg",
-    projectId: "101124",
-    modelId: "qc_101124_173657_4",
-    sysscreenHeight: ""
+    button_text: "上传照片",  //首页按钮内容显示“上传照片”
+    image_src: "../../libs/img/woman.jpg", //替换为你要融合前的模型图片地址
+    image_src_list: [],  //图片预览地址，请留空
+    projectId: "101124", //腾讯云人脸融合活动ID
+    modelId: "qc_101124_173657_4", //腾讯云人脸融合素材ID
+    sysscreenHeight: "" //自适应手机高度，请留空
   },
 
-  uploadImage() {
+  uploadImage() { //图片上传函数，由前端上传照片按钮调用
     var myThis = this;
-    wx.chooseImage({
-      count: 1,
-      sizeType: ['compressed'],
-      sourceType: ['album', 'camera'],
-      success(chooseImage_res) {
-        wx.showLoading({
+    wx.chooseImage({ //微信小程序选择图片API
+      count: 1, //最多可以选择的图片张数
+      sizeType: ['compressed'], //所选的图片的尺寸，这里选择压缩图
+      sourceType: ['album', 'camera'], //选择图片的来源，可从相机和相册
+      success(chooseImage_res) { //接口调用成功的回调函数
+        wx.showLoading({ //显示 loading 提示框API
           title: '生成中...',
         })
-        wx.getFileSystemManager().readFile({
-          filePath: chooseImage_res.tempFilePaths[0], //选择图片返回的相对路径
-          encoding: 'base64', //编码格式
-          success(base64_res) {
-            wx.cloud.callFunction({
-              name: "FaceMerge",
-              data: {
-                base64: base64_res.data,
-                projectId: myThis.data.projectId,
-                modelId: myThis.data.modelId
+        wx.getFileSystemManager().readFile({ //获取全局唯一的文件管理器API，并用readFile读取本地文件内容
+          filePath: chooseImage_res.tempFilePaths[0], //要读取的文件的路径，这里是本地上传图片文件的临时目录
+          encoding: 'base64', //指定读取文件的字符编码，这里指定“base64”
+          success(base64_res) { //接口调用成功的回调函数
+            wx.cloud.callFunction({ //调用云函数API
+              name: "FaceMerge", //云函数名
+              data: { //传递给云函数的参数
+                base64: base64_res.data, //readFile中生成的base64编码文件
+                projectId: myThis.data.projectId, //data中指定的projectId
+                modelId: myThis.data.modelId //data中指定的modelId
               },
-              success(cloud_callFunction_res) {
-                wx.hideLoading()
-                wx.showToast({
-                  title: '成功',
-                  icon: 'success',
-                  duration: 500
+              success(cloud_callFunction_res) { //接口调用成功的回调函数
+                wx.hideLoading() //隐藏 loading 提示框
+                wx.showToast({ //显示消息提示框
+                  title: '成功', //	提示的内容
+                  icon: 'success', //图标
+                  duration: 500 //提示的延迟时间
                 })
-                console.log(cloud_callFunction_res)
                 myThis.setData({
-                  image_src: cloud_callFunction_res.result.Image,
-                  button_text: "重新生成"
+                  image_src: cloud_callFunction_res.result.Image, //将云函数返回的URL地址写在data中
+                  button_text: "重新生成" //按钮变为“重新生成”
                 })
               },
-              fail(err) {
-                console.log(err)
-                wx.hideLoading()
-                wx.showModal({
-                  title: '失败',
+              fail(err) { //接口调用失败的回调函数
+                console.log(err) //控制台打印错误
+                wx.hideLoading() //隐藏 loading 提示框
+                wx.showModal({ //显示消息提示框
+                  title: '失败', //提示的内容
                   content: "人脸融合失败，请重试！"
                 })
               }
@@ -57,12 +57,12 @@ Page({
     })
   },
 
-  onLoad: function() {
+  onLoad: function () { //微信生命周期函数，小程序载入中执行
     var myThis = this;
-    wx.getSystemInfo({
-      success(res) {
+    wx.getSystemInfo({ //获取系统信息
+      success(res) { //接口调用成功的回调函数
         myThis.setData({
-          sysscreenHeight: res.screenHeight - 660
+          sysscreenHeight: res.screenHeight - 660 //获取屏幕高度接口
         })
       }
     })
