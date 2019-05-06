b6b8244986de0de9912147af0ab1feb88a22764d
Techeek <qc_957a692741d9444b9f25c53a3da69e7a@git.code.tencent.com>
2019-05-06 22:25:31 +0800

    image

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -6,7 +6,7 @@ Page({
     image_mark_here: "image_mark_here",
     image_mark_leave: "image_mark_leave",
     projectId: "101124",
-    modelId: "qc_101124_192757_5",
+    modelId: "qc_101124_221646_13",
     sysscreenHeight: ""
   },
 
@@ -34,14 +34,14 @@ Page({
               image_src: "../../libs/img/woman.jpg",
               image_mark_here: "image_mark_leave",
               image_mark_leave: "image_mark_here",
-              modelId: "qc_101124_192759_6"
+              modelId: "qc_101124_221650_14"
             })
           } else {
             myThis.setData({
               image_src: "../../libs/img/man.jpg",
               image_mark_here: "image_mark_here",
               image_mark_leave: "image_mark_leave",
-              modelId: "qc_101124_192757_5"
+              modelId: "qc_101124_221646_13"
             })
           }
         }, 1200)
